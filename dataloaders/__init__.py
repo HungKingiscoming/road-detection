f17a32836b7a0268e292e3b0e35e022ce3db8212
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2026-08-05 16:01:25 +0700

    Create __init__.py

---
+++ dataloaders/__init__.py
@@ -0,0 +1,15 @@
+from dataloaders.datasets import spacenet
+from torch.utils.data import DataLoader
+
+def make_data_loader(args, **kwargs):
+    if args.dataset == 'spacenet':
+        train_loader, val_loader = spacenet.build_spacenet_dataloaders(
+            data_dir=args.data_dir,
+            batch_size=args.batch_size,
+            num_workers=kwargs.get('num_workers', 4)
+        )
+        test_loader = None
+        num_classes = 2
+        return train_loader, val_loader, test_loader, num_classes
+    else:
+        raise NotImplementedError(f"Dataset {args.dataset} chưa được hỗ trợ!")
