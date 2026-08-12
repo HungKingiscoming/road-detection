cb703017f8ffd0e9e1062c43a6adc636a954ca42
Giang Tuấn Hùng <123647321+HungKingiscoming@users.noreply.github.com>
2026-08-12 16:27:39 +0700

    Delete modeling/backbone directory

--- modeling/backbone/__init__.py
+++
@@ -1,7 +0,0 @@
-from modeling.backbone import resnet
-
-def build_backbone(backbone, output_stride, BatchNorm, pretrained=True):
-    if backbone == 'resnet':
-        return resnet.ResNet101(output_stride, BatchNorm, pretrained=pretrained)
-    else:
-        raise NotImplementedError
